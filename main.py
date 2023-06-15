475b2823f20f99e29f91f9afbc558ca6710c99fa
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-06-16 00:44:09 +0500

    update

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from transliterate import to_cyrillic, to_latin
+import telebot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+TOKEN = '6281796769:AAHQEx0eth_mHj257nfrCUQM_YofsOWWGG0'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    answer = 'Hello, Welcome👋'
+    answer += '\nEnter text'
+    bot.reply_to(message, answer)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    msg = message.text
+    answer = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, answer(msg))
+
+
+bot.polling()
